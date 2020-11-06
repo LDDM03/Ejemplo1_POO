c58030d903c43c213f950dc779ea7c91fa97c927
LDDM03 <62411508+LDDM03@users.noreply.github.com>
2020-11-06 03:14:10 -0400

    Update Persona.java

--- Persona.java
+++ Persona.java
@@ -1,4 +1,14 @@
+  
+/**************************************************************************
+ * Programa:Persona.java
+ * Objetivo: Dar ejemplo basico de atributos,metodos y clase (POO);
+ * @author: Luis Diaz ci: 29.763.189
+ * @since: 06/11/2020
+ * @version:1.0
+ * Persona.java: Realizado por Luis Diaz
+ *************************************************************************/
 public class Persona {
+	
 	
 	String nombre,apellido;
 	int edad;
